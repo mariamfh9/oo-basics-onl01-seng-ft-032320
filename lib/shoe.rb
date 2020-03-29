f0945e354825dd192afe318e6565ee47b86ee9f2
Mariam Haider <mfh.haider@gmail.com>
2020-03-29 00:27:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,28 @@
 # Make your shoe class here!
 class Shoe
   def initialize(brand)
+    @brand = brand
   end 
+  def brand 
+    @brand
+  end 
+  def color=(color)
+    @color = color
+  end
+  def color
+    @color
+  end 
+  def size=(size)
+     @size = size
+  end 
+  def size
+    @size
+  end 
+  def material=(material)
+    @material=material
+  end 
+  def material
+      
+
 end 
 Shoe.new("Adidas")
